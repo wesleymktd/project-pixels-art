15cbddcec4378239a849b226c677aaeb8f83d7b7
wesleymktd <wesleymktd@gmail.com>
2022-08-31 19:13:39 -0300

    terminei o 2

---
+++ script.js
@@ -0,0 +1,22 @@
+const paletColor = document.querySelector('#color-palette')
+// cada cor contida nessa paleta de cores
+
+const colorPalet1 = document.createElement('div');
+colorPalet1.className = 'color'
+colorPalet1.id = 'color1'
+paletColor.appendChild(colorPalet1);
+
+const colorPalet2 = document.createElement('div');
+colorPalet2.className = 'color'
+colorPalet2.id = 'color2'
+paletColor.appendChild(colorPalet2);
+
+const colorPalet3 = document.createElement('div');
+colorPalet3.className = 'color'
+colorPalet3.id = 'color3'
+paletColor.appendChild(colorPalet3);
+
+const colorPalet4 = document.createElement('div');
+colorPalet4.className = 'color'
+colorPalet4.id = 'color4'
+paletColor.appendChild(colorPalet4);
