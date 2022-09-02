1193efc4c9d86914ef0af44119e32e8879d562f2
wesleymktd <wesleymktd@gmail.com>
2022-09-02 00:04:09 -0300

    terminei5 certo

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ paletColor.appendChild(colorPalet4);
 
 // função para mudança de cor ao clicar muda paleta 2
 
+var arr = [];
+
 let button = document.getElementById('button-random-color')
   function generateColor2() {
 
@@ -32,7 +34,8 @@ let button = document.getElementById('button-random-color')
     color += letters[Math.floor(Math.random() * 16)]
   }  
   colorPalet2.style.backgroundColor = color;
-  localStorage.setItem('corPaleta2',color); //aqui eu armazeno em localStore essa cor que foi gerada no clique
+  arr.push(color);
+  localStorage.colorPalette = JSON.stringify(arr); //aqui eu armazeno em localStore essa cor que foi gerada no clique
 }
 
 button.addEventListener('click', generateColor2 );
@@ -47,7 +50,8 @@ function generateColor3() {
     color += letters[Math.floor(Math.random() * 16)]
   }  
   colorPalet3.style.backgroundColor = color;
-  localStorage.setItem('corPaleta3',color);
+  arr.push(color);
+  localStorage.colorPalette = JSON.stringify(arr)
 }
 
 button.addEventListener('click', generateColor3 )
@@ -62,24 +66,43 @@ function generateColor4() {
     color += letters[Math.floor(Math.random() * 16)]
   }  
   colorPalet4.style.backgroundColor = color;
-  localStorage.setItem('corPaleta4',color);
+  arr.push(color);
+  localStorage.colorPalette = JSON.stringify(arr)
 }
 
 button.addEventListener('click', generateColor4 )
 
-// vou recuperar minha cor assim que a página for carregada e jogar para o elemento da paleta
-
-function recoverColor() {
-  colorPalet2.style.backgroundColor = localStorage['corPaleta2']
-  colorPalet3.style.backgroundColor = localStorage['corPaleta3']
-  colorPalet4.style.backgroundColor = localStorage['corPaleta4']
+if(localStorage.colorPalette) {
+  let colorGeneral = JSON.parse(localStorage.getItem('colorPalette'));
+  
+  colorPalet2.style.backgroundColor = colorGeneral[0];
+  colorPalet3.style.backgroundColor = colorGeneral[1];
+  colorPalet4.style.backgroundColor = colorGeneral[2];  
 }
-recoverColor()
+
+// vou recuperar minha cor assim que a página for carregada e jogar para o elemento da paleta
 
 // 1 - tem que ter algum lugar na função que eu armazene a informação de cor quando ela for acionada no clique
 // e jogar para dentro de localStorage   ok
 // 2 - agora só falta eu conseguir recuperar essa informação e jogar pra dentro da paleta ao carregar minha página
 // 2-1 consigo recuperar através do localStorage[corPaleta2]
-   
+/*   
+const myObj = {
+  name: 'Link',
+  age: 20,
+};
 
+// Precisamos transformar este objeto em uma string. Para isso, utilizamos a função `JSON.stringify()`.
 
+localStorage.setItem('myData', JSON.stringify(myObj));
+
+// Como dito anteriormente, o `localStorage` armazena dados no formato chave e valor, sendo os valores sempre string. Para isso, precisamos passar como primeiro parâmetro do método `setItem()` o nome da chave, passamos `myData` e como segundo parâmetro, a função `JSON.stringify()` com o objeto-valor que queremos armazenar na chave `myData`. Feito isso, teremos salvo no `localStorage` um objeto com a chave `myData` e o valor `{ "name": "Link", "age": 20 }`.
+
+// Agora, precisamos recuperar o objeto armazenado, mas queremos ele no formato original e não como uma string. Para isso, utilizaremos a função `JSON.parse()`.
+
+const recoveredObject = JSON.parse(localStorage.getItem('myData'));
+
+// Utilizamos a função `JSON.parse()` para transformar a string que está armazenada no `localStorage` de volta para seu formato original, um objeto.
+
+console.log(recoveredObject); // { name: 'Link', age: 20 }
+*/
